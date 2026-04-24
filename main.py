0ae3a2c0851b3906fbd52323524aef49aa5be7ec
Heberton Henrique Coronel da Silva <heberton.coronel@outlook.com>
2026-04-23 21:51:26 -0300

    segunda parte

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 import os
 import time
+from tarefas import adicionar_tarefa, listar_tarefas, apagar_tarefa, concluir_tarefa
 
-TEMPO = 3
+TEMPO = 5 
 
-def mostrar_opcoes():
-    print("1- Adicionar")
+while True:
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+  
+    print("\n#### MENU ####")
+    print("\n1- Adicionar")
     print("2- Listar")
-    print("3- Apagar tarefa")
-    print("4- Sair")
+    print("3- Concluir Tarefa")
+    print("4- Apagar Tarefa")
+    print("5- Sair")
+    
+    opcao = input("\nEscolha uma opção: ")
 
+    match opcao:
+        case "1":
+            desc = input("Descrição da tarefa: ")
+            adicionar_tarefa(desc)
+            time.sleep(5)
+        
+        case "2":
+            listar_tarefas()
+            time.sleep(5)
+        
+        case "3":
+            listar_tarefas() 
+            idx = input("\nDigite o número da tarefa para concluir: ")
+            concluir_tarefa(idx)
+            time.sleep(5)
+            
+        case "4":
+            listar_tarefas()
+            idx = input("\nDigite o número da tarefa para apagar: ")
+            apagar_tarefa(idx)
+            time.sleep(5)
+            
+        case "5":
+            print("Saindo... Obrigado por utilizar nosso sistema!")
+            time.sleep(5)
+            break
+            
+        case _:
+            print("Opção inválida!")
+            time.sleep(5)
--- tarefas.py
+++ tarefas.py
@@ -2,20 +2,37 @@ lista_tarefas = []
 
 def adicionar_tarefa(descricao):
 
-    tarefa = {
+    tarefas = {
         "descricao": descricao,
         "concluida": False
     }
-    lista_tarefas.append(tarefa)
+    lista_tarefas.append(tarefas)
     print(f"\n Tarefa '{descricao}' adicionada com sucesso!")
 
-    for tarefa in lista_tarefas
-
-
+    
 def listar_tarefas():
-    print("Lista de tarefas")
-    if lista_tarefas:
-        print("A lista está vazia.")
+    print("\nLista de tarefas")
+    if not lista_tarefas:
+        print("\nA lista está vazia.")
     else:
-        for tarefa in lista_tarefas:
-            print(f"executar {tarefa}['descricao']")  
+        for indice, tarefa in enumerate(lista_tarefas, 1):
+            status = "[X]" if tarefa["concluida"] else "[ ]"
+            print(f"\nIndice:{indice}. {status}Tarefa:{tarefa['descricao']}") 
+
+def concluir_tarefa(indice_usuario):
+    try:
+        # 2. A Atualização: converte e acessa o índice
+        indice = int(indice_usuario) - 1
+        lista_tarefas[indice]["concluida"] = True
+        print(f"\n Tarefa '{lista_tarefas[indice]['descricao']}' marcada como concluída!")
+    
+    except (IndexError, ValueError):
+        print("\n Erro: Índice inválido! Verifique o número na lista.")
+
+def apagar_tarefa(indice_usuario):
+    try:
+        indice = int(indice_usuario) - 1
+        removida = lista_tarefas.pop(indice)
+        print(f"\n Tarefa '{removida['descricao']}' removida!")
+    except (IndexError, ValueError):
+        print("\ Erro: Não foi possível apagar. Índice inexistente.")
